5bbcd87f43db3007451d3f20c9b5827ce185c8c3
devkumar27 <dev270502@gmail.com>
2024-04-27 10:10:26 +0530

    minor: building files

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,24 +13,24 @@ const Login = () => {
   });
   const { email, password } = inputValue;
 
-  useEffect(() => {
-    const verifyCookie = async () => {
-      if (!cookies.token) {
-        navigate("/login");
-      }
-      const { data } = await axios.post(
-        "https://trash-tracker-gb4c.onrender.com",
-        {},
-        { withCredentials: true }
-      );
-      // const { status, isAdmin } = data;
-      // setUsername(user);
-      return data.status
-        ? navigate('/')
-        : (removeCookie("token"), navigate("/login"));
-    };
-    verifyCookie();
-  })
+  // useEffect(() => {
+  //   const verifyCookie = async () => {
+  //     if (!cookies.token) {
+  //       navigate("/login");
+  //     }
+  //     const { data } = await axios.post(
+  //       "https://trash-tracker-gb4c.onrender.com",
+  //       {},
+  //       { withCredentials: true }
+  //     );
+  //     // const { status, isAdmin } = data;
+  //     // setUsername(user);
+  //     return data.status
+  //       ? navigate('/')
+  //       : (removeCookie("token"), navigate("/login"));
+  //   };
+  //   verifyCookie();
+  // })
 
   const handleOnChange = (e) => {
     const { name, value } = e.target;
